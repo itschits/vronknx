5d6ba79fb746fe839f0643e965b81cccdbbdbf40
Chittaranjan Pradhan <chitta.ranjan.24@gmail.com>
2025-06-05 10:19:03 -0400

    New fix for look and feel

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -56,7 +56,7 @@ function Chatbot() {
             )}
             {messages.length !== 0 && (<header className='sticky top-0 shrink-0 z-20'>
                 <div className='flex flex-col h-full w-full gap-1 pt-4 pb-4'>
-                    <h1 className='font-barclay text-[1.65rem] backdrop-blur-lg font-bold text-blue-300'>Bot Squad Chatbot 1.0</h1>
+                    <h1 className='font-barclay text-[1.65rem] backdrop-blur-lg font-bold text-blue-300'>Sage Chatbot 1.0</h1>
                 </div>
             </header>)}
 
